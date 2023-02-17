828f6e0ab8c9cae45ee124960c7510d48276bf5b
alex-v-10 <alex.ru9311@gmail.com>
2023-02-18 00:06:01 +0300

    style header

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,11 @@
-/* в этот файл добавляет скрипты*/
+const menu = document.querySelector('.menu');
+const menuButton = document.querySelector('.navigation__menu-button');
+
+menu.classList.remove('menu--active');
+menuButton.classList.remove('navigation__menu-button--active');
+
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('menu--active');
+  menuButton.classList.toggle('navigation__menu-button--active');
+});
+
