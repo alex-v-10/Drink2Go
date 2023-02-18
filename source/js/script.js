63c19b0e0969e7ec1856f6d7f8dfcb3e12e7efea
alex-v-10 <alex.ru9311@gmail.com>
2023-02-18 13:49:19 +0300

    style promo

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,5 @@
+/* Activate Menu */
+
 const menu = document.querySelector('.menu');
 const menuButton = document.querySelector('.navigation__menu-button');
 
@@ -7,5 +9,29 @@ menuButton.classList.remove('navigation__menu-button--active');
 menuButton.addEventListener('click', () => {
   menu.classList.toggle('menu--active');
   menuButton.classList.toggle('navigation__menu-button--active');
+});
+
+/* Promo Slider */
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  // loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
 
